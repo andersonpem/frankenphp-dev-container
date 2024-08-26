894d6194ab29938f9b90181df7dbc913c4b9b23a
Anderson Souza <sza.andersonsilva@gmail.com>
2024-08-26 16:19:18 +0100

    WIP

--- docker/sh/aliases.sh
+++ docker/sh/aliases.sh
@@ -3,10 +3,13 @@ alias cda="composer dump-autoload"
 alias dumpvars="composer dump-env $APP_ENV"
 alias cachereset="bin/console doctrine:cache:clear-metadata && bin/console doctrine:cache:clear-query && bin/console doctrine:cache:clear-result && bin/console app:cache:clear && sudo rm -Rf $PROJECT_ROOT/var/cache/*"
 alias phpunit="php bin/phpunit"
+alias pa="php artisan"
 aliases_help() {
     echo -e "${cl_info}Helper aliases:${cl_reset}"
     echo -e "${cl_success}bcs${cl_reset} 'php bin/console' - Runs a PHP command within the bin/console directory."
     echo -e "${cl_success}cda${cl_reset} 'composer dump-autoload' - Dumps the autoloader using Composer."
     echo -e "${cl_success}cda${cl_reset} 'composer dump-env \$APP_ENV' - Dumps the environment variables set for the application."
     echo -e "${cl_success}cachereset${cl_reset} 'bin/console cache:reset' - Resets all the cache using a console command."
+    echo -e "${cl_success}phpunit${cl_reset} 'php bin/phpunit' - Runs PHPUnit tests."
+    echo -e "${cl_success}pa${cl_reset} 'php artisan' - Runs Laravel Artisan commands."
 }
