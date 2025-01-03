4a9e60a24c825ad472c2e43705d8519cd17773ed
AndersonPEM <sza.andersonsilva@gmail.com>
2025-01-03 09:45:49 +0000

    Local pipeline for build

--- local_build_base.sh
+++ local_build_base.sh
@@ -4,15 +4,27 @@ set -ex -o pipefail
 # PHP 8.2
 docker buildx build \
   --build-arg PHP_VERSION=8.2 \
+  --build-arg FRANKENPHP_VERSION=1.3 \
   --platform linux/amd64,linux/arm64 \
-  --tag phillarmonic/frankenphp-workspace:1.2-php-8.2 \
+  --tag phillarmonic/frankenphp-workspace:1.3-php-8.2 \
   --file docker/Dockerfile \
   --push .
 
+# PHP 8.3
 docker buildx build \
   --build-arg PHP_VERSION=8.3 \
+  --build-arg FRANKENPHP_VERSION=1.3 \
   --platform linux/amd64,linux/arm64 \
-  --tag phillarmonic/frankenphp-workspace:1.2-php-8.3 \
+  --tag phillarmonic/frankenphp-workspace:1.3-php-8.3 \
+  --file docker/Dockerfile \
+  --push .
+
+# PHP 8.4
+docker buildx build \
+  --build-arg PHP_VERSION=8.4 \
+  --build-arg FRANKENPHP_VERSION=1.3 \
+  --platform linux/amd64,linux/arm64 \
+  --tag phillarmonic/frankenphp-workspace:1.3-php-8.4 \
   --file docker/Dockerfile \
   --push .
 
